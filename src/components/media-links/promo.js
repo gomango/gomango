b42e20a460792669b89720c2637a8033ba85bb5a
gomango <81490859+gomango@users.noreply.github.com>
2021-04-11 12:49:19 -0400

    Update promo.js

--- src/components/media-links/promo.js
+++ src/components/media-links/promo.js
@@ -1,4 +1,5 @@
 export const videos = [
   { src: "https://player.vimeo.com/video/518204943" },
   { src: "https://player.vimeo.com/video/505046110" },
+  { src: "https://player.vimeo.com/video/511190460" },
 ];
