3543a321847c23f26df556107986857680b2f4eb
gomango <81490859+gomango@users.noreply.github.com>
2021-04-11 12:34:06 -0400

    Update events.js

--- src/components/media-links/events.js
+++ src/components/media-links/events.js
@@ -1,4 +1,5 @@
 export const videos = [
   { src: "https://player.vimeo.com/video/518208505" },
   { src: "https://player.vimeo.com/video/518204466" },
+ { src: "https://player.vimeo.com/video/522111836" },
 ];
