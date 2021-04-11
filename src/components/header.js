679a59d6953c07824e267738f6ea9e339fa97434
gomango <81490859+gomango@users.noreply.github.com>
2021-04-11 16:15:06 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="container-fluid mb-5">
+    <div className="mb-5">
       <div>
         <img className="img-fluid" src="/background.jpg" />
       </div>
