ef92b56c981df5fa028f6f922aca0517b65c8e88
gomango <81490859+gomango@users.noreply.github.com>
2021-06-07 22:23:39 -0400

    Update weddings.js

--- src/components/media-links/weddings.js
+++ src/components/media-links/weddings.js
@@ -5,15 +5,15 @@ export const videos = [
   },
   {
     src:
+      "https://player.vimeo.com/video/508258604?color=ffffff&title=0&byline=0",
+  },
+  {
+    src:
       "https://player.vimeo.com/video/510495937?color=ffffff&title=0&byline=0",
   },
   {
     src:
       "https://player.vimeo.com/video/509971080?color=ffffff&title=0&byline=0",
-  },
-  {
-    src:
-      "https://player.vimeo.com/video/508258604?color=ffffff&title=0&byline=0",
   },
   {
     src:
