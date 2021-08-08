f9fe20b4d019b9ff6b53919af4811db5124109dd
gomango <81490859+gomango@users.noreply.github.com>
2021-08-08 12:03:51 -0500

    Update media.js

--- src/components/media.js
+++ src/components/media.js
@@ -11,22 +11,7 @@ function Media() {
   return (
     // <div className="row justify-content-end text-light">
     <div className="col col-sm-3 d-flex mb-3">
-      <a
-        href="https://www.instagram.com/anatoly_kuteliya/"
-        target="_blank"
-        rel="noreferrer"
-        className="mr-3"
-      >
-        <i class="fab fa-instagram fa-2x"></i>
-      </a>
 
-      <a
-        href="https://vimeo.com/user129958158"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <i class="fab fa-vimeo fa-2x"> </i>
-      </a>
 
       {/* <i class="fab fa-facebook-square fa-2x"></i> */}
 
