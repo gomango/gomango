8e9bfe246e1dde91c51666d04d616b1cd9a5466e
gomango <81490859+gomango@users.noreply.github.com>
2022-03-13 22:58:02 -0400

    Update music.js

--- src/components/media-links/music.js
+++ src/components/media-links/music.js
@@ -1,6 +1,10 @@
 export const videos = [
   {
     src:
+      "https://player.vimeo.com/showcase/9358435/video/687780301,
+  },
+  {
+    src:
       "https://player.vimeo.com/video/518204943?color=ffffff&title=0&byline=0",
   },
   {
