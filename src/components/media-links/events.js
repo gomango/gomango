81312bc42c6ad246fe78534865e65f131ab1ea8e
gomango <81490859+gomango@users.noreply.github.com>
2022-06-07 22:13:24 -0400

    Update events.js

--- src/components/media-links/events.js
+++ src/components/media-links/events.js
@@ -1,6 +1,10 @@
 export const videos = [
   {
     src:
+      "https://player.vimeo.com/video/718139396?color=ffffff&title=0&byline=0",
+  },
+  {
+    src:
       "https://player.vimeo.com/video/525867687?color=ffffff&title=0&byline=0",
   },
   {
