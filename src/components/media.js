598ab050dc6802d1117178eba063666bfe1f3ad1
gomango <81490859+gomango@users.noreply.github.com>
2023-01-11 21:49:00 -0600

    Update instagram

--- src/components/media.js
+++ src/components/media.js
@@ -4,7 +4,7 @@ import React from "react";
 // import FacebookIcon from '@material-ui/icons/Facebook';
 // import YouTubeIcon from '@material-ui/icons/YouTube';
 // import DuoIcon from '@material-ui/icons/Duo';
-// import Grid from '@material-ui/core/Grid';
+
 // Update
 
 function Media() {
