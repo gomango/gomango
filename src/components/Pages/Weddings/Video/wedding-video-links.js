c7d9c608e519d6e9793b3830a5e50fa131a6f290
gomango <81490859+gomango@users.noreply.github.com>
2023-02-26 20:04:18 -0500

    Update wedding-video-links.js

--- src/components/Pages/Weddings/Video/wedding-video-links.js
+++ src/components/Pages/Weddings/Video/wedding-video-links.js
@@ -1,35 +1,5 @@
 export const weddingVideos = [
   {
-    video: "https://vimeo.com/661773243",
-    poster: "screenshots/weddings/dmitry_tanya.JPG",
-    sub: "<p>Wedding.</p>",
-    alt: "dmitry_tanya",
-  },
-  {
-    video: "https://vimeo.com/508258604",
-    poster: "screenshots/weddings/eren_mine.JPG",
-    sub: "<p>Wedding.</p>",
-    alt: "eren_mine",
-  },
-  {
-    video: "https://vimeo.com/700320594",
-    poster: "screenshots/weddings/ersin_karina.JPG",
-    sub: "<p>Wedding.</p>",
-    alt: "ersin_karina",
-  },
-  {
-    video: "https://vimeo.com/560005866",
-    poster: "screenshots/weddings/igor_karina.JPG",
-    sub: "<p>Wedding.</p>",
-    alt: "igor_karina",
-  },
-  {
-    video: "https://vimeo.com/577246764",
-    poster: "screenshots/weddings/marina.JPG",
-    sub: "<p>Wedding.</p>",
-    alt: "marina",
-  },
-  {
     video: "https://vimeo.com/791893560",
     poster: "screenshots/weddings/michele_katy.JPG",
     sub: "<p>Wedding.</p>",
@@ -54,9 +24,40 @@ export const weddingVideos = [
     alt: "ralph_samantha",
   },
   {
+    video: "https://vimeo.com/560005866",
+    poster: "screenshots/weddings/igor_karina.JPG",
+    sub: "<p>Wedding.</p>",
+    alt: "igor_karina",
+  },
+  {
+    video: "https://vimeo.com/577246764",
+    poster: "screenshots/weddings/marina.JPG",
+    sub: "<p>Wedding.</p>",
+    alt: "marina",
+  },
+  
+  {
     video: "https://vimeo.com/522109188",
     poster: "screenshots/weddings/surprise_proposal_bryan.JPG",
     sub: "<p>Wedding.</p>",
     alt: "surprise_proposal_bryan",
+  },
+  {
+    video: "https://vimeo.com/661773243",
+    poster: "screenshots/weddings/dmitry_tanya.JPG",
+    sub: "<p>Wedding.</p>",
+    alt: "dmitry_tanya",
+  },
+  {
+    video: "https://vimeo.com/508258604",
+    poster: "screenshots/weddings/eren_mine.JPG",
+    sub: "<p>Wedding.</p>",
+    alt: "eren_mine",
+  },
+  {
+    video: "https://vimeo.com/700320594",
+    poster: "screenshots/weddings/ersin_karina.JPG",
+    sub: "<p>Wedding.</p>",
+    alt: "ersin_karina",
   },
 ];
