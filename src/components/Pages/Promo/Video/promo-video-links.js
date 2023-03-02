76e3479fad2c3a9ff9e422eb9466cddae993314c
gomango <81490859+gomango@users.noreply.github.com>
2023-03-02 13:09:49 -0500

    Update promo-video-links.js

--- src/components/Pages/Promo/Video/promo-video-links.js
+++ src/components/Pages/Promo/Video/promo-video-links.js
@@ -3,27 +3,44 @@ export const promoVideos = [
   {
     video: "https://vimeo.com/718690456",
     poster: "screenshots/promo/groove_cruise.JPG",
-    sub: "<p>Collection of handmade accessories.</p>",
+    sub: "<p> Highlights of 2022 Groove cruise.</p>",
     alt: "groove_cruise",
   },
   {
     video: "https://vimeo.com/661682931",
     poster: "screenshots/promo/handmade_collection.JPG",
-    sub: "<p>Promo.</p>",
+    sub: "<p> Collection of Handmade accesories.</p>",
     alt: "handmade_collection",
   },
+  
   {
-    video: "https://vimeo.com/761274084",
-    poster: "screenshots/promo/anastasia_bday.JPG",
+    video: "https://vimeo.com/718140162",
+    poster: "screenshots/promo/promo_dance.JPG",
     sub: "<p>Promo.</p>",
-    alt: "anastasia_bday",
-  }, 
+    alt: "promo_dance",
+  },
+  
+  {
+    video: "https://vimeo.com/518204466",
+    poster: "screenshots/promo/how_to_be_wow.JPG",
+    sub: "<p>Highlights from "How to be WOW" presentation.</p>",
+    alt: "how_to_be_wow",
+  },
+  
   {
     video: "https://vimeo.com/521882226",
     poster: "screenshots/promo/burning_man_2.JPG",
     sub: "<p>Promo.</p>",
     alt: "burning_man_2",
   },
+  
+  {
+    video: "https://vimeo.com/538210261",
+    poster: "screenshots/promo/elegant_barbershop.JPG",
+    sub: "<p>Promo.</p>",
+    alt: "elegant_barbershop",
+  },
+  
   {
     video: "https://vimeo.com/518208505",
     poster: "screenshots/promo/burning_man_1.JPG",
@@ -31,45 +48,24 @@ export const promoVideos = [
     alt: "burning_man_1",
   },
   {
-    video: "https://vimeo.com/538210261",
-    poster: "screenshots/promo/elegant_barbershop.JPG",
-    sub: "<p>Promo.</p>",
-    alt: "elegant_barbershop",
-  },
-  {
     video: "https://vimeo.com/522111836",
     poster: "screenshots/promo/holi.JPG",
     sub: "<p>Promo.</p>",
     alt: "holi",
   },
-  {
-    video: "https://vimeo.com/518204466",
-    poster: "screenshots/promo/how_to_be_wow.JPG",
-    sub: "<p>Promo.</p>",
-    alt: "how_to_be_wow",
-  },
+  
   {
     video: "https://vimeo.com/718139396",
     poster: "screenshots/promo/jameson.JPG",
     sub: "<p>Promo.</p>",
     alt: "jameson",
   },
-  {
-    video: "https://vimeo.com/718140162",
-    poster: "screenshots/promo/promo_dance.JPG",
-    sub: "<p>Promo.</p>",
-    alt: "promo_dance",
-  },
+  
   {
     video: "https://vimeo.com/525867687",
     poster: "screenshots/promo/purple_yam.JPG",
     sub: "<p>Promo.</p>",
     alt: "purple_yam",
   },
-  {
-    video: "https://vimeo.com/763623635",
-    poster: "screenshots/promo/july_anniversary.JPG",
-    sub: "<p>Promo.</p>",
-    alt: "july_anniversary",
-  },
+  
 ];
