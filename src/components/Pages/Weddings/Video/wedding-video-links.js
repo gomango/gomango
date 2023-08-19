134f2206a94efcbca6f4a83ba44da0edbdcb2898
gomango <81490859+gomango@users.noreply.github.com>
2023-08-18 20:40:12 -0400

    Update wedding-video-links.js

--- src/components/Pages/Weddings/Video/wedding-video-links.js
+++ src/components/Pages/Weddings/Video/wedding-video-links.js
@@ -1,4 +1,10 @@
 export const weddingVideos = [
+ {
+    video: "https://vimeo.com/855883148",
+    poster: "screenshots/weddings/Dillon and Katerina1.JPG",
+    sub: "<p>Wedding.</p>",
+    alt: "Anton and Aniuta",
+  },
   {
     video: "https://vimeo.com/840268551",
     poster: "screenshots/weddings/Dillon and Katerina1.JPG",
