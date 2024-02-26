9737b2100a17311078f7b36e429518d87d2f25e2
gomango <81490859+gomango@users.noreply.github.com>
2024-02-25 20:09:43 -0500

    Update promo-video-links.js

--- src/components/Pages/Promo/Video/promo-video-links.js
+++ src/components/Pages/Promo/Video/promo-video-links.js
@@ -49,58 +49,10 @@ export const promoVideos = [
   },
   
   {
-    video: "https://vimeo.com/718140162",
-    poster: "screenshots/promo/promo_dance.JPG",
-    sub: "<p>Promo.</p>",
-    alt: "promo_dance",
-  },
-  
-  {
-    video: "https://vimeo.com/518204466",
-    poster: "screenshots/promo/how_to_be_wow.JPG",
-    sub: "<p>Highlights from How to be WOW presentation.</p>",
-    alt: "how_to_be_wow",
-  },
-  
-  {
-    video: "https://vimeo.com/521882226",
-    poster: "screenshots/promo/burning_man_2.JPG",
-    sub: "<p>Promo.</p>",
-    alt: "burning_man_2",
-  },
-  
-  {
     video: "https://vimeo.com/538210261",
     poster: "screenshots/promo/elegant_barbershop.JPG",
     sub: "<p>Promo.</p>",
     alt: "elegant_barbershop",
-  },
-  
-  {
-    video: "https://vimeo.com/518208505",
-    poster: "screenshots/promo/burning_man_1.JPG",
-    sub: "<p>Promo.</p>",
-    alt: "burning_man_1",
-  },
-  {
-    video: "https://vimeo.com/522111836",
-    poster: "screenshots/promo/holi.JPG",
-    sub: "<p>Promo for Holi Festival_NYC.</p>",
-    alt: "holi",
-  },
-  
-  {
-    video: "https://vimeo.com/718139396",
-    poster: "screenshots/promo/jameson.JPG",
-    sub: "<p>Promo.</p>",
-    alt: "jameson",
-  },
-  
-  {
-    video: "https://vimeo.com/525867687",
-    poster: "screenshots/promo/purple_yam.JPG",
-    sub: "<p>Promo.</p>",
-    alt: "purple_yam",
   },
   
 ];
